d35b283ee399e5c5324256d37f8c96afd3277b02
btea <2356281422@qq.com>
2018-10-09 11:11:07 +0800

    alert button

---
+++ src/app.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="body">
+        <Button><span>this is a span</span></Button>
+        <Button type="primary" title="文字">登录</Button>
+        <Button type="primary" ghost :title="news"></Button>
+        <Button type="warning"></Button>
+        <Button type="warning" ghost></Button>
+        <Button type="error">自定义按钮</Button>
+        <Button type="error" ghost>自定义按钮，样式修改</Button>
+        <Alert num="12">
+            <div style="color: #6cf;" class="con">
+                <p>第一个</p>
+                <p style="color: #000;">第二个</p>
+            </div>
+            <div>456</div>
+            弹窗
+        </Alert>
+    </div>
+</template>
+
+<script>
+// 局部注册
+// import Button from './button/button'
+// console.log(Button);
+export default {
+    data(){
+        return {
+            news: 'no game no life'
+        }
+    }
+    // 局部注册组件
+    // components: {
+        // Button: Button
+    // },
+}
+</script>
+
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './app'
+
+// import 'element-ui/lib/theme-chalk/index.css'
+import Button from './button'
+import Alert from './alert'
+import './style/index.less'
+
+// 全局注册
+Vue.component('Button',Button);
+Vue.component('Alert',Alert);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
